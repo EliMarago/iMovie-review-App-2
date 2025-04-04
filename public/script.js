730f2e8cca5341f165aeb955398f6263213d0837
Elisabetta <156212818+EliMarago@users.noreply.github.com>
2025-04-04 18:52:41 +0200

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,61 @@
+
+const imgPath = `https://image.tmdb.org/t/p/w1280`;
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
+
+async function getData(url) {
+  const response = await fetch(`/api/movies?query=${query}`);
+  const data = await response.json();
+
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = ""; // Pulisce la lista
+
+  movies.forEach(movie => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie")
+    movieEl.innerHTML = `
+    <img
+    src="${imgPath + poster_path}"
+    alt="${title}"/>
+  <div class="movie-info">
+    <h3>${title}</h3>
+    <span class="${getClassRate(vote_average)}">${vote_average.toFixed(2)}
+  </span>
+  </div>
+  <div class="overview">
+    <h3>${title}</h3>
+    ${overview}
+  </div>`;
+    main.appendChild(div);
+  });
+}
+function getClassRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
+getData("popular"); // Carica film popolari all'inizio
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchTerms = search.value;
+
+  if (searchTerms && searchTerms !== "") {
+    getData(searchTerms);
+    search.value = "";
+  } else {
+    alert("Inserisci una parola chiave!");
+  }
+});
---
+++ server/index.js
@@ -0,0 +1,23 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function(event, context) {
+  const apiKey = process.env.API_KEY; // La tua chiave API è recuperata dalla variabile d'ambiente di Netlify
+  const query = event.queryStringParameters.query || "popular"; // Parametro di ricerca o "popular"
+
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Errore durante la chiamata all'API." })
+    };
+  }
+};
